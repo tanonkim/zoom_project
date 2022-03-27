5a758d4b828908dfd05eee7b15097d07b1df16df
tanonkim <robbins4bos@gmail.com>
2022-03-27 19:23:14 +0900

    add : websocket messages

--- src/server.js
+++ src/server.js
@@ -19,6 +19,13 @@ function handleConnection(socket) {
   console.log(socket);
 }
 
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message.toString("utf8"));
+  });
+  socket.send("hello!!!");
+});
 
 server.listen(3000, handleListen);
