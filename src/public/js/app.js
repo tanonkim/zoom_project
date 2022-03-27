6601cac1f6b7e0b6ecdbab1bb67cbfc5260941e2
tanonkim <robbins4bos@gmail.com>
2022-03-27 21:34:17 +0900

    add : chat function

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 function handleOpen() {
@@ -16,6 +18,11 @@ socket.addEventListener("message", (message) => {
 
 socket.addEventListener("close", handleClose);
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 10000);
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
